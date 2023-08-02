94cefbfe56bfbae0c3d83d7b43de5e15a3861796
gabriel barbosa <daviflamengo067@gmail.com>
2023-08-02 18:48:25 -0300

    inicio do trabalho(modulo menu)

---
+++ menu.js
@@ -0,0 +1,16 @@
+
+function menu(n){
+    while(true){
+    console.log("2-cadastro de aluno")
+    console.log("3-listar todos os alunos")
+    console.log("4-listar aluno em específico")             
+    console.log("5-atualizar aluno")            
+    console.log("6-excluir aluno")            
+    console.log("7-media")
+    console.log("0-sair")
+    n = input("digite uma opcao: ")
+    if(n == 0){
+        break
+    }}
+}          
+menu()
