0360cb323c590c594f1695a484d481812f06d24f
gabriel barbosa <daviflamengo067@gmail.com>
2023-08-07 20:42:32 -0300

    modulo cadastro(id)

--- cadastro.js
+++ cadastro.js
@@ -1,12 +1,20 @@
 const write =  require("fs").writeFileSync
 const input=require('readline-sync').question;
+const read =  require("fs").readFileSync
 trabalho = 0
+id=0
 function cadrastroo(){
-      a = input("digite o nome: ")
-      b = input("digite a matricula: ")
-      c = input("digite a idade: ")
-      d = input("digite a nota: ")
-      trabalho = a+";"b+";"c+";"d+";"
+       b = input("digite o nome: ")
+       c = input("digite a matricula: ")
+       d = input("digite a idade: ")
+       e = input("digite a nota: ")
+         
+       conteudo = read("trabalho.csv", {encoding: "utf-8"})
+      if(conteudo=="sep=;\nid;nome;matricula;idade;nota"){
+            id=1
+      }
+      
+      trabalho = "\n"+id+";"+b+";"+c+";"+d+";"+e+";"
        write("trabalho.csv",trabalho.toString(), {flag: "a"});
 }
 
